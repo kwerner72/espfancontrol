4ca1682ad7eb43722a1d3e984c1913f030bd6f52
Kai Werner <kai.werner@etecture.de>
2020-09-27 14:20:00 +0200

    first beta version

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# espfancontrol
+# ESP Fan Control
+Software to control a fan by temperature using a PID controller
+
+Required libraries:
+- ArduinoJson (https://arduinojson.org/?utm_source=meta&utm_medium=library.properties)
+- DallasTemperature (https://github.com/milesburton/Arduino-Temperature-Control-Library)
+- DHT Sensor Library (https://github.com/adafruit/DHT-sensor-library)
+- OLED SSD1306 (https://github.com/durydevelop/arduino-lib-oled)
+- OneWire (https://www.pjrc.com/teensy/td_libs_OneWire.html)
+- PID (https://playground.arduino.cc/Code/PIDLibrary/)
--- fan_control.ino
+++ fan_control.ino
@@ -1,176 +1,208 @@
 #include <ESP8266WiFi.h>
-
-// WifiManager
-#include <DNSServer.h>
-#include <ESP8266WebServer.h>
-#include <WiFiManager.h>
-
-// Temp sensor
 #include <OneWire.h>
 #include <DallasTemperature.h>
-
-// Display
+#include <DHT.h>
 #include <Wire.h>
-#include "OLED.h"
+#include <oled.h>
+#include <PID_v1.h>
 
-// MQTT
-#include <PubSubClient.h>
+#include <UrlTokenBindings.h>
+#include <RichHttpServer.h>
 
-#define INTERVAL      5000
-#define FAN_PIN       4
-#define ONE_WIRE_BUS  2
+// WiFi
+#define WIFI_SSID     ssid
+#define WIFI_PWD      pwd
 
+// fans
+#define PWM_PIN       2  // D4
+#define TACH_PIN      14 // D5
+
+// DS18B20 temp sensor
+#define ONE_WIRE_BUS  12 // D6
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
+double Temp_Out;
 
-OLED display(2, 14);
+// DHT22 temp sensor
+#define DHT_SENSOR_PIN  13 // D7
+#define DHT_TYPE        DHT22
+double Temp_In;
+DHT dht(DHT_SENSOR_PIN, DHT_TYPE);
 
-WiFiClient espClient;
-PubSubClient client(espClient);
+// SSD1306 display
+#define SDA_PIN       4
+#define SCL_PIN       5
+#define I2C_ADDRESS   0x3C
+#define RES_X         128
+#define RES_Y         64
+#define IS_SSD1306    true
+OLED display(SDA_PIN, SCL_PIN, NO_RESET_PIN, I2C_ADDRESS, RES_X ,RES_Y, IS_SSD1306);
 
-const char* MQTT_BROKER = "BROKER_IP";
-const int MQTT_BROKER_PORT = 1883;
+// PID controller
+#define PID_P   20
+#define PID_I   5
+#define PID_D   5
+#define CALCULATION_PERIOD 1000 //Number of milliseconds over which to interrupts
+double SetPoint, PIDValue;
+PID myPID(&Temp_Out, &PIDValue, &SetPoint, PID_P, PID_I, PID_D, P_ON_M, DIRECT);
 
-int lastMillis = 0;
-float set_temperature = 50.0;
+volatile int interruptCounter; //counter use to detect hall sensor in fan
+unsigned long previousMillis;    
+
+using RichHttpConfig = RichHttp::Generics::Configs::EspressifBuiltin;
+using RequestContext = RichHttpConfig::RequestContextType;
+SimpleAuthProvider authProvider;
+RichHttpServer<RichHttpConfig> server(80, authProvider);
+
+
+void ICACHE_RAM_ATTR handleInterrupt() { //This is the function called by the interrupt
+  if(digitalRead(TACH_PIN)==0) interruptCounter++;
+}
 
 void setup() {
+  // init console
   Serial.begin(115200);
 
-  sensors.begin();
-
-  WiFiManager wifiManager;
-
-  wifiManager.setBreakAfterConfig(true);
-
-  if (!wifiManager.autoConnect("AutoConnectAP", "password")) {
-    Serial.println("failed to connect, we should reset as see if it connects");
-    delay(3000);
-    ESP.reset();
-    delay(5000);
+  DeviceAddress devAddr;
+  if (!sensors.getAddress(devAddr, 0)) {
+    Serial.println("No DS18B20 device found");
+  } else {
+    sensors.setResolution(devAddr, 11);
   }
+  sensors.begin();
+  dht.begin();
 
-  Serial.println("local ip");
+  // init temp & PID
+  Temp_In = analogRead(0);
+  SetPoint = 31;
+  myPID.SetMode(AUTOMATIC);
+  myPID.SetOutputLimits(0,1023);
+  myPID.SetSampleTime(CALCULATION_PERIOD);
+  myPID.SetControllerDirection(REVERSE);
+
+  // init display
+  display.begin();
+
+  WiFi.begin(WIFI_SSID, WIFI_SSID);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Waiting to connect...");
+  }
   Serial.println(WiFi.localIP());
 
-  client.setServer(MQTT_BROKER, MQTT_BROKER_PORT);
+  server.buildHandler("/temp/in").on(HTTP_GET, handleTempInGET);
+  server.buildHandler("/temp/out").on(HTTP_GET, handleTempOutGET);
+  server.buildHandler("/speed").on(HTTP_GET, handleSpeedGET);
+  server.buildHandler("/target").on(HTTP_GET, handleTargetGET);
+  server.buildHandler("/target/:value").on(HTTP_PUT, handleTargetPUT);
+  server.clearBuilders();
+  server.begin();
 
-  display.begin();
+  // init fan speed and reading
+  previousMillis = 0;
+  interruptCounter = 0;
+  pinMode(TACH_PIN, INPUT_PULLUP);
+  analogWriteFreq(10000);  
+  analogWrite(PWM_PIN, 0); // start with lowest speed
+  attachInterrupt(digitalPinToInterrupt(TACH_PIN), handleInterrupt, FALLING);
+
 }
 
 void loop() {
 
-  if (abs(millis() - lastMillis) > INTERVAL) {
+  if((millis() - previousMillis) > CALCULATION_PERIOD) {// Process counters once every second
+    previousMillis = millis();
+    int count = interruptCounter;
+    interruptCounter = 0;
+
     sensors.requestTemperatures();
-    float temp = sensors.getTempCByIndex(0);
-    int pidValue = calcPID(temp);
-    
-    analogWrite(FAN_PIN, 1023 - pidValue);
-
-    displayData(temp, pidValue);
-    logData(temp, pidValue);
-    publishData(temp, pidValue);
-
-
-    lastMillis = millis();
-  }
-}
-
-void reconnect() {
-  while (!client.connected()) {
-    Serial.print("Reconnecting to MQTT broker...");
-    if (!client.connect("ProjectorClient")) {
-        Serial.print("failed, rc=");
-        Serial.print(client.state());
-        Serial.println(" retrying in 5 seconds");
-        delay(5000);
+    Temp_Out = sensors.getTempCByIndex(0);
+    if (Temp_Out < 0) {
+      Temp_Out = 0;
     }
+
+    Temp_In = dht.readTemperature();
+    if (isnan(Temp_In)) {
+      Temp_In = 0;
+    }
+
+    if (myPID.Compute()) {
+      analogWrite(PWM_PIN, PIDValue);
+    }
+    
+    displayData(count);
+    logData(count, PIDValue);
   }
+
+  server.handleClient();
+
 }
 
-void displayData(float temp, int speed) {
-  char buf[25];
-  sprintf(buf, "Current temp: %f", temp);
+void displayData(int speed) {
   display.clear();
-  display.print(buf);
+  
+  char bufTempIn[25];
+  sprintf(bufTempIn, "I: %.1f°C", Temp_In);
+  display.draw_string(0, 0, bufTempIn, OLED::DOUBLE_SIZE);
+  
+  char bufTempOut[25];
+  sprintf(bufTempOut, "O: %.1f°C", Temp_Out);
+  display.draw_string(0, 24, bufTempOut, OLED::DOUBLE_SIZE);
+  
+  char bufSpeed[25];
+  int rpm = 2 * speed / 3;
+  sprintf(bufSpeed, "%d RPM", rpm);
+  display.draw_string(0, 48, bufSpeed, OLED::DOUBLE_SIZE);
+  display.display();
 }
 
-void logData(float temp, int speed) {
-  Serial.print("Current Temperature: ");
-  Serial.println(temp);
-
-  Serial.print("Current Fanspeed: ");
-  Serial.println(speed);
+void logData(int speed, double pidValue) {
+  Serial.print("TIn: ");
+  Serial.print(Temp_In);
+  Serial.print("°C / TOut: ");
+  Serial.print(Temp_Out);
+  Serial.print("°C / Speed: ");
+  int rpm = 2 * speed / 3;
+  Serial.print((int)rpm);
+  Serial.print("RPM / ");
+  Serial.println(pidValue);
 }
 
-void publishData(float temp, int speed) {
-  if (!client.connected()) {
-    reconnect();
-  }
-  client.loop();
-
-  char tempMsg[3];
-  sprintf(tempMsg, "%f", temp);
-  client.publish("/projector/temp", tempMsg);
-
-  char pidMsg[4];
-  sprintf(pidMsg, "%d", speed);
-  client.publish("/projector/fan/speed", pidMsg);
+void handleTempInGET(RequestContext& requestContext) {
+  handleGetTemp(Temp_In);
 }
 
-////////////////////////////////////////////////////////////////
-// from https://www.electronoobs.com/eng_arduino_tut24_code3.php
+void handleTempOutGET(RequestContext& requestContext) {
+  handleGetTemp(Temp_Out);
+}
 
-//PID constants
-///////////////////////////////////////////////////////////////
-int kp = 90;
-int ki = 30;
-int kd = 80;
-///////////////////////////////////////////////////////////////
-float elapsedTime, Time, timePrev;
-float temperature_read;
-float previous_error = 0;
-float PID_value = 0;
-float PID_error = 0;
-int PID_p = 0;
-int PID_i = 0;
-int PID_d = 0;
-float last_kp = 0;
-float last_ki = 0;
-float last_kd = 0;
+void handleGetTemp(double temp) {
+  char bufTemp[25];
+  sprintf(bufTemp, "%.1f", temp);
+  server.send(200, "text/plain", bufTemp);
+}
 
-int calcPID(float currentTemp) {
-  // First we read the real value of temperature
-  temperature_read = currentTemp;
-  //Next we calculate the error between the setpoint and the real value
-  PID_error = set_temperature - temperature_read + 3;
-  //Calculate the P value
-  PID_p = 0.01 * kp * PID_error;
-  //Calculate the I value in a range on +-3
-  PID_i = 0.01 * PID_i + (ki * PID_error);
+void handleSpeedGET(RequestContext& requestContext) {
+  char bufSpeed[25];
+  int rpm = 2 * PIDValue / 3;
+  sprintf(bufSpeed, "%d", rpm);
+  server.send(200, "text/plain", bufSpeed);
+}
 
+void handleTargetGET(RequestContext& requestContext) {
+  char bufSetPoint[25];
+  sprintf(bufSetPoint, "%.1f", SetPoint);
+  server.send(200, "text/plain", bufSetPoint);
+}
 
-  //For derivative we need real time to calculate speed change rate
-  timePrev = Time;                            // the previous time is stored before the actual time read
-  Time = millis();                            // actual time read
-  elapsedTime = (Time - timePrev) / 1000;
-  //Now we can calculate the D calue
-  PID_d = 0.01 * kd * ((PID_error - previous_error) / elapsedTime);
-  //Final total PID value is the sum of P + I + D
-  PID_value = (PID_p + PID_i + PID_d) * 4;
+void handleTargetPUT(RequestContext& request) {
+  double value = atof(request.pathVariables.get("value"));
+  Serial.print("New SetPoint: ");
+  Serial.println(value);
 
-  //We define PWM range between 0 and 255
-  if(PID_value < 0)
-  {
-    PID_value = 0;
-  }
-  if(PID_value > 1023)
-  {
-    PID_value = 1023;
-  }
-  //Now we can write the PWM signal to the mosfet on digital pin D3
-  //Since we activate the MOSFET with a 0 to the base of the BJT, we write 255-PID value (inverted)
-  previous_error = PID_error;     //Remember to store the previous error for next loop.
+  SetPoint = value;
 
-  return PID_value;
+  request.response.json["success"] = true;
+  request.response.setCode(200);
+  
 }
